3c6b89989882c2bb88641f691add6be7cb3b374c
srikanth141199 <kolleparasrikanth@gmailcom>
2023-08-02 20:03:36 +0530

    BirthdayList using usestate

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import data from './data'
+
+const App = () => {
+
+  const [birthList, setbirthlist] = useState(data)
+  const Handeler = () =>{
+    setbirthlist([]);
+  }
+  return (
+    <main >
+      <section className='container'>
+      <h3>Birthday Buddy</h3>
+      {(birthList.map((obj1)=>{
+        return(
+          <section className='person' key={obj1.id}>
+            <img src={obj1.image} alt={obj1.name} />
+            <h4>{obj1.name}</h4>
+            <p>{obj1.age} years</p>
+          </section>
+        )
+      }))}
+      <button onClick={Handeler} className='btn'>Clear All</button>
+      </section>
+    </main>
+  );
+};
+export default App;
+
